c63e12e4564f8f2476b4f2392609c1173c756a5e
zilani131 <zilani.cuet@gmail.com>
2022-03-15 23:46:58 +0600

    passing object property  as parameter

--- js/brain.js
+++ js/brain.js
@@ -9,19 +9,47 @@ const displayCountries=country=>{
     // console.log(country.name.common)
     const containerCountries=document.getElementById("countries")
     containerCountries.innerHTML=countryHtml.join(' ')
+    // join(' ') is used to avoid comma when mapping 
 }
+// option 2
+// here we pass the object property as parameter
+// as we see in option 1 countrys ={name,flags}
+//  so we can directly pass object property as parameter
+const countryContainer=({name,flags})=>{
+           
+        return `
+        <div class="card">
+        <div class="card-child"><h2>${name.common}</h2>
+        <img src="${flags.png}"></div>
+        </div>
+    
+        `
+    }
 
+// option-1
+// const countryContainer=country=>{
+//         const {name,flags}=country
+//     return `
+//     <div class="card">
+//     <div class="card-child"><h2>${name.common}</h2>
+//     <img src="${flags.png}"></div>
+//     </div>
 
-const countryContainer=country=>{
+//     `
+// }
+
+// orginal
+
+// const countryContainer=country=>{
    
-    return `
-    <div class="card">
-    <div class="card-child"><h2>${country.name.common}</h2>
-    <img src="${country.flags.png}"></div>
-    </div>
+//     return `
+//     <div class="card">
+//     <div class="card-child"><h2>${country.name.common}</h2>
+//     <img src="${country.flags.png}"></div>
+//     </div>
 
-    `
-}
+//     `
+// }
 
 
 
